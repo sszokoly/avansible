fc4fa0d7a559f50b22840bcaa7258f795998f605
root <root@rocky9.lab.local>
2023-03-27 21:55:54 -0600

    sql

--- main.sql
+++ main.sql
@@ -4,10 +4,8 @@ DROP TABLE ansible_child_group;
 DROP TABLE ansible_parent_group;
 DROP TABLE ansible_access_group;
 
-
 CREATE TABLE ansible_parent_group(
-   id INTEGER PRIMARY KEY,
-   group_name TEXT,
+   group_name TEXT PRIMARY KEY,
    descr TEXT,
    vendor TEXT
 );
@@ -36,25 +34,21 @@ VALUES
 
 
 CREATE TABLE ansible_child_group(
-   id INTEGER PRIMARY KEY,
-   group_name TEXT,
+   group_name TEXT PRIMARY KEY,
    descr TEXT,
-   parent_group_id INTEGER NOT NULL,
-   FOREIGN KEY (parent_group_id)
-      REFERENCES ansible_parent_group(id)
-      ON DELETE CASCADE
-      ON UPDATE CASCADE
+   parent_group_name TEXT NOT NULL,
+   FOREIGN KEY (parent_group_name) REFERENCES ansible_parent_group(group_name)
 );
 
-INSERT INTO ansible_child_group (group_name,descr,parent_group_id)
+INSERT INTO ansible_child_group (group_name,descr,parent_group_name)
 VALUES
-    ("smgrpri","Primary System Manager",1),
-    ("smgrgeo","Geo System Manager",1),
-    ("asm","Core Session Manager",2),
-    ("bsm","Branch Session Manager",2),
-    ("cmmain","Main Communication Manager",3),
-    ("cmess","ESS Communication Manager",3),
-    ("cmlsp","LSP Communication Manager",3);
+    ("smgrpri","Primary System Manager","SMGR"),
+    ("smgrgeo","Geo System Manager","SMGR"),
+    ("asm","Core Session Manager","SM"),
+    ("bsm","Branch Session Manager","SM"),
+    ("cmmain","Main Communication Manager","CM"),
+    ("cmess","ESS Communication Manager","CM"),
+    ("cmlsp","LSP Communication Manager","CM");
 
 CREATE TABLE ansible_access_group(
    id INTEGER NOT NULL PRIMARY KEY,
@@ -79,8 +73,8 @@ CREATE TABLE host(
    id INTEGER NOT NULL PRIMARY KEY,
    inventory_hostname TEXT NOT NULL UNIQUE,
    ansible_host TEXT NOT NULL,
-   ansible_parent_group_id TEXT NOT NULL,
-   ansible_child_group_id TEXT,
+   ansible_parent_group_name TEXT NOT NULL,
+   ansible_child_group_name TEXT,
    ansible_access_group_id INTEGER,
    ansible_user TEXT,
    ansible_password TEXT,
@@ -90,24 +84,16 @@ CREATE TABLE host(
    ansible_connection TEXT DEFAULT "ssh",
    ansible_port INTEGER DEFAULT 22,
    ssh_args TEXT,
-   FOREIGN KEY (ansible_parent_group_id)
-      REFERENCES ansible_parent_group(id)
-      ON UPDATE CASCADE
-      ON DELETE RESTRICT
-   FOREIGN KEY (ansible_child_group_id)
-      REFERENCES ansible_child_group(id)
-      ON UPDATE CASCADE
-      ON DELETE SET NULL
-   FOREIGN KEY (ansible_access_group_id)
-      REFERENCES ansible_access_group(id)
-      ON UPDATE CASCADE
-      ON DELETE SET NULL
+   FOREIGN KEY (ansible_parent_group_name) REFERENCES ansible_parent_group(group_name)
+   FOREIGN KEY (ansible_child_group_name) REFERENCES ansible_child_group(group_name)
+   FOREIGN KEY (ansible_access_group_id) REFERENCES ansible_access_group(id)
 );
 
-INSERT INTO host (inventory_hostname,ansible_host,ansible_parent_group_id,ansible_child_group_id,ansible_user,ansible_password,ansible_become_pass)
+INSERT INTO host (inventory_hostname,ansible_host,ansible_parent_group_name,ansible_child_group_name,ansible_user,ansible_password,ansible_become_pass)
 VALUES
-    ("smgr","192.168.11.101",1,1,"combat","cmb@Dm1n","cmb@Dm1n"),
-    ("sm","192.168.11.102",2,3,"combat","cmb@Dm1n","cmb@Dm1n");
+    ("smgr","192.168.11.101","SMGR","smgrpri","combat","cmb@Dm1n","cmb@Dm1n"),
+    ("sm","192.168.11.102","SM","asm","combat","cmb@Dm1n","cmb@Dm1n");
+
 
 -- DROP TABLE ansible_parent_group;
 -- DELETE FROM ansible_parent_group;
--- orm/initdb.py
+++ orm/initdb.py
@@ -18,27 +18,21 @@ class Ansible_Child_Group(Base):
 
     group_name =  Column(String(), primary_key=True)
     descr = Column(String())
-    parent_group_name = Column(
-        String(),
-        ForeignKey(
-            'ansible_parent_group.group_name', 
-            ondelete='CASCADE',
-            onupdate='CASCADE'
-        ),
-        nullable=True,
-    )
+    parent_group_name = Column(String(), ForeignKey('ansible_parent_group.group_name'), nullable=True)
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-#parent1 = Ansible_Parent_Group(group_name="SMGR", descr="System Manager", vendor="Avaya")
-#parent2 = Ansible_Parent_Group(group_name="SM", descr="Session Manager", vendor="Avaya")
+parent1 = Ansible_Parent_Group(group_name="SMGR", descr="System Manager", vendor="Avaya")
+parent2 = Ansible_Parent_Group(group_name="SM", descr="Session Manager", vendor="Avaya")
 child1 = Ansible_Child_Group(group_name="asm", descr="Core Session Manager", parent_group_name="SM")
-#child2 = Ansible_Child_Group(group_name="SM", descr="Session Manager", vendor="Avaya")
+child2 = Ansible_Child_Group(group_name="bsm", descr="Branch Session Manager", parent_group_name="SM")
 
-#session.add(parent1)
-#session.add(parent2)
+session.add(parent1)
+session.add(parent2)
 session.add(child1)
+session.add(child2)
 session.commit()
