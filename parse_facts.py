3279bfc3468d22a3f948bd936783b34a07747825
Szabolcs Szokoly <sszokoly@protonmail.com>
2023-04-05 18:36:38 -0400

    update

--- parse_facts.py
+++ parse_facts.py
@@ -12,6 +12,7 @@
 import os
 import sys
 import yaml
+from utils.certificate_info import certificate_info
 
 FACTS_CACHE = "facts_cache"
 
@@ -20,7 +21,18 @@ def main(facts_cache):
     for file in files:
         with open(os.path.join(facts_cache, file), 'r') as fd:
             facts = yaml.safe_load(fd)
-            print(file, facts.keys())
+            group_names = facts.get('group_names', None)
+            if not group_names:
+                continue
+            ansible_parent_group = group_names[0]
+            certificates = facts.get('certificates', None)
+            if not certificates:
+                print (ansible_parent_group, "", "") 
+            else:
+                for service, pem_list in certificates.items():
+                    if not pem_list:
+                        continue
+                    print(file, ansible_parent_group, service, certificate_info(pem_list[0]))
 
 if __name__ == "__main__":
     facts_cache = sys.argv[1] if len(sys.argv) > 1 else FACTS_CACHE
--- utils/certificate_info.py
+++ utils/certificate_info.py
@@ -13,30 +13,62 @@ import sys
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.x509.oid import ExtensionOID
+from cryptography.hazmat.backends import default_backend
+
+def load_pem_data(pem_data):
+    pem_data = pem_data.encode() if isinstance(pem_data, str) else pem_data
+    try:
+        cert = x509.load_pem_x509_certificate(pem_data, backend=default_backend())
+    except:
+        print("PEM_DATA: {0}".format(pem_data))
+        cert = None
+    return cert
+
+def get_subject_cn(cert):
+    scns = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
+    if scns:
+        return scns[0].value
+    return ""
+
+def get_subject_o(cert):
+    sos = cert.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)
+    if sos:
+        return sos[0].value
+    return ""
+
+def get_issuer_cn(cert):
+    icns = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
+    if icns:
+        return icns[0].value
+    return ""
+
+def get_issuer_o(cert):
+    ios = cert.issuer.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)
+    if ios:
+        return ios[0].value
+    return ""
+
+def get_san(cert):
+    try:
+        ext = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+    except:
+        return ""
+    san_dns = [str(x) for x in ext.value.get_values_for_type(x509.DNSName)]
+    san_ip = [str(x) for x in ext.value.get_values_for_type(x509.IPAddress)]
+    return ','.join(san_dns + san_ip)
 
 def certificate_info(pem_data):
-    pem_data = pem_data.encode() if isinstance(pem_data, str) else pem_data
-    cert = x509.load_pem_x509_certificate(pem_data)
-    subject_cn = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
-    subject_o = cert.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
-    issuer_cn = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
-    issuer_o = cert.issuer.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
-    not_valid_before = cert.not_valid_before
-    not_valid_after = cert.not_valid_after
-    serial_number = hex(cert.serial_number)
-    ext = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
-    san_dns = ext.value.get_values_for_type(x509.DNSName)
-    san_ip = ext.value.get_values_for_type(x509.IPAddress)
+    cert = load_pem_data(pem_data)
 
     return {
-        'subject_cn': subject_cn,
-        'subject_o': subject_o,
-        'issuer_cn': issuer_cn,
-        'issuer_o': issuer_o,
-        'not_valid_before': not_valid_before,
-        'not_valid_after': not_valid_after,
-        'serial_number': serial_number,
-        'subject_alternative_name': ','.join(san_dns + san_ip),
+        'subject_cn': get_subject_cn(cert) if cert else "",
+        'subject_o': get_subject_o(cert) if cert else "",
+        'issuer_cn': get_issuer_cn(cert) if cert else "",
+        'issuer_o': get_issuer_o(cert) if cert else "",
+        'not_valid_before': cert.not_valid_before if cert else "",
+        'not_valid_after': cert.not_valid_after if cert else "",
+        'serial_number': hex(cert.serial_number) if cert else "",
+        'subject_alternative_name': get_san(cert) if cert else "",
     }
 
 if __name__ == "__main__":
